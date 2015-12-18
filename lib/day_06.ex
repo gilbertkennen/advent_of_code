8e13a15b0747c340b3e75335eca4b086af66eb62
Gilbert Kennen <gilbert@firewatcher.org>
2015-12-17 21:35:48 -0800

    major Day06 refactor and improvement

--- lib/day_06.ex
+++ lib/day_06.ex
@@ -1,76 +1,159 @@
 defmodule Day06 do
 
-  def execute(filename) do
+  def part_one(filename) do
     filename
     |> parse_file
-    |> Enum.reduce([], fn
-      {action, reg}, on_regions -> act(action, reg, on_regions)
+    |> Enum.reverse
+    |> Enum.reduce([], fn region, regions ->
+      add_region(region, regions, &combine_bool/2)
+      |> Enum.sort_by(&(-size(&1)))
     end)
-    |> sum
+    |> sum_part_one
   end
 
 
-  def parse_file(filename) do
-    import String, only: [to_integer: 1]
+  def sum_part_one(regions) do
+    Enum.reduce(regions, 0, fn
+      {:off, _}, acc -> acc
+      region, acc -> acc + size(region)
+    end)
+  end
+
+  def part_two(filename) do
     filename
-    |> File.stream!
-    |> Stream.map(fn line ->
-      [_, action, from_x, from_y, to_x, to_y] =
-        Regex.run(~r/(toggle|on|off) (\d+),(\d+) through (\d+),(\d+)/, line)
+    |> parse_file
+    |> Enum.map(fn
+      {:on, coords}     -> {1,  coords}
+      {:off, coords}    -> {-1, coords}
+      {:toggle, coords} -> {2,  coords}
+    end)
+    |> Enum.reduce([], fn region, regions ->
+      add_region(region, regions, &combine_lum/2)
+      |> Enum.sort_by(&(-size(&1)))
+    end)
+    |> sum_part_two
+  end
 
-      {String.to_atom(action),
-       {to_integer(from_x)..to_integer(to_x),
-       to_integer(from_y)..to_integer(to_y)}
-      }
+  def sum_part_two(regions) do
+    Enum.reduce(regions, 0, fn
+      {val, _}, acc when val <= 0 -> acc
+      {val, _} = region, acc ->
+        acc + size(region) * val
     end)
   end
 
+  def size({_, {x..xx, y..yy}}), do: ((xx - x + 1) * (yy - y + 1))
 
-  def sum(on_regions) do
-    Enum.reduce(on_regions, 0, fn
-      {x..xx, y..yy}, acc -> acc + ((xx - x + 1) * (yy - y + 1))
-    end)
+
+  def add_region(region, regions, fun) do
+    add_region([region], regions, [], fun)
   end
 
-
-  # Subtract reg from all existing regions.
-  def act(:off, reg, on_regions) do
-    on_regions
-    |> Enum.flat_map(&subtract(&1, reg))
-  end
-
-
-  # Subtract reg from all and place it in the output.
-  def act(:on, reg, on_regions) do
-    [reg | act(:off, reg, on_regions)]
-  end
-
-
-  # Manually subtract reg from all existing regions, saving the
-  # intersections and then subtract those from the toggle intersection
-  # and keep whatever is left.
-  def act(:toggle, reg, on_regions) do
-    {new_grid, intersections} =
-      Enum.reduce(on_regions, {[], []}, fn old, {acc, intersections} ->
-        if intersection = two_dim_intersection(reg, old) do
-          old_mask = negative(old, intersection)
-          {old_mask ++ acc, [intersection | intersections]}
-        else
-          {[old | acc], intersections}
-        end
+  def add_region(new, [], acc, _), do: :lists.reverse(new, acc)
+  def add_region(new, [region | regions], acc, fun) do
+    {new, intersections, existing} =
+      Enum.reduce(new, {[], [], []}, fn n_reg, n_acc ->
+        fun.(n_reg, region) |> combine_accumulators(n_acc)
       end)
 
-    Enum.reduce(intersections, [reg], &act(:off, &1, &2)) ++ new_grid
+    acc =
+      subtract_multi(region, intersections)
+      |> :lists.reverse(existing)
+      |> :lists.reverse(acc)
+
+    add_region(new, regions, acc, fun)
   end
 
 
-  # Given two arbitrary regions, subtract the second from the first.
+  # Toggles nullify their intersection
+  def combine_bool({:toggle, coords_l}, {:toggle, coords_r}) do
+    {new_l, intersection} = subtract(coords_l, coords_r)
+    {
+      regions(new_l, :toggle),
+      intersection,
+      []
+    }
+  end
+  # existing toggles flip the intersection, which is then done.
+  def combine_bool({type, coords_l}, {:toggle, coords_r}) do
+    {new_l, toggled} = subtract(coords_l, coords_r)
+
+    {
+      regions(new_l, type),
+      toggled,
+      :lists.reverse(regions(toggled, flip(type))),
+    }
+  end
+  # existing non-toggles are already set.
+  def combine_bool({type_l, coords_l}, {_, coords_r}) do
+    {new_l, _} = subtract(coords_l, coords_r)
+
+    {regions(new_l, type_l), [], []}
+  end
+
+
+  # This algorithm can put negative regions into the existing area, but they
+  # shouldn't count, so delete them.
+  def combine_lum(l, {val_r, coords_r}) when val_r <= 0 do
+    {[l], [coords_r], []}
+  end
+
+  # When we add a negative region, we should look for something to reduce.
+  # This is intentionally a bit more generic than it has to be in order to
+  # handle other luminosity systems.
+  def combine_lum({val_l, coords_l}, {val_r, coords_r}) when val_l < 0 do
+    {new_l, intersection} = subtract(coords_l, coords_r)
+    i_val = val_l + val_r
+    new_l =
+      if i_val < 0 do
+        (regions(new_l, val_l) ++ regions(intersection, i_val))
+      else
+        regions(new_l, val_l)
+      end
+
+    {
+      new_l,
+      intersection,
+      (if i_val > 0, do: regions(intersection, i_val), else: [])
+    }
+  end
+
+  # If it is a positive value, just add an extra region.
+  def combine_lum(l, _) do
+    { [l], [], [], }
+  end
+
+
+  def combine_accumulators({l_acc1, l_acc2, l_acc3}, {r_acc1, r_acc2, r_acc3}) do
+    {
+      :lists.reverse(l_acc1, r_acc1),
+      :lists.reverse(l_acc2, r_acc2),
+      :lists.reverse(l_acc3, r_acc3)
+    }
+  end
+
+  def flip(:on), do: :off
+  def flip(:off), do: :on
+
+
+  def regions(coords, value) do
+    for coord <- coords, do: {value, coord}
+  end
+
   def subtract(l, r) do
     if intersection = two_dim_intersection(l, r) do
-      negative(l, intersection)
+      {negative(l, intersection), [intersection]}
     else
-      [l]
+      {[l], []}
     end
+  end
+
+  def subtract_multi({type, coords}, intersections) do
+    intersections
+    |> Enum.reduce([coords], fn i, r ->
+      Enum.flat_map(r, &(subtract(&1, i) |> elem(0)))
+    end)
+    |> regions(type)
   end
 
 
@@ -102,129 +185,21 @@ defmodule Day06 do
     if min <= max, do: min..max, else: nil
   end
 
-end
 
-defmodule Day06_2 do
-  import Day06, only: [parse_file: 1,
-                             two_dim_intersection: 2,
-                             negative: 2,
-                             subtract: 2]
-
-
-  def execute(filename) do
+  def parse_file(filename) do
+    import String, only: [to_integer: 1]
     filename
-    |> parse_file
-    |> Enum.reduce([], fn
-      {action, reg}, on_regions -> act(action, reg, on_regions)
-    end)
-    |> sum
-  end
+    |> File.stream!
+    |> Stream.map(fn line ->
+      [_, action, from_x, from_y, to_x, to_y] =
+        Regex.run(~r/(toggle|on|off) (\d+),(\d+) through (\d+),(\d+)/, line)
 
-  def sum(regions) do
-    regions
-    |> Enum.reduce(0, fn {{x..xx, y..yy}, val}, acc ->
-      acc + ((xx - x + 1) * (yy - y + 1) * val)
+      {String.to_atom(action),
+       {to_integer(from_x)..to_integer(to_x),
+        to_integer(from_y)..to_integer(to_y)}
+      }
     end)
   end
 
-  def act(:on, reg, regions) do
-    do_stuff({reg, 1}, regions)
-  end
-
-  def act(:toggle, reg, regions) do
-    do_stuff({reg, 2}, regions)
-  end
-
-  def act(:off, reg, regions) do
-    do_stuff({reg, -1}, regions)
-  end
-
-  def do_stuff({reg, val}, regions) do
-    {new_regions, intersections} =
-      Enum.reduce(regions, {[], []}, fn
-        {o_reg, o_val}, {new_regions, intersections}->
-          if inter = two_dim_intersection(reg, o_reg) do
-            new_reg = negative(o_reg, inter) |> Enum.map(&{&1, o_val})
-            if (n_val = o_val + val) > 0, do: new_reg = [{inter, n_val}| new_reg]
-            {new_reg ++ new_regions , [inter | intersections]}
-          else
-            {[{o_reg, o_val} | new_regions], intersections}
-          end
-      end)
-
-    if val > 0 do
-      intersections
-      |> Enum.reduce([reg], &Enum.flat_map(&2, fn r -> subtract(r, &1) end))
-      |> Enum.map(&{&1, val})
-      |> Kernel.++(new_regions)
-    else
-      new_regions
-    end
-  end
-
-end
-
-defmodule VariableLights do
-  import Day06, only: [parse_file: 1]
-
-
-  def execute(filename) do
-    table = init()
-
-    filename
-    |> parse_file
-    |> Enum.each(fn {action, ranges} ->
-      ranges
-      |> block
-      |> Enum.each(&act(action, table, &1))
-    end)
-
-    result = sum(table)
-    cleanup(table)
-    result
-  end
-
-
-  def block({x_range, y_range}) do
-    for x <- x_range, y <- y_range, do: {x, y}
-  end
-
-
-  def act(:on, table, coords) do
-    inc(table, coords, 1)
-  end
-
-  def act(:toggle, table, coords) do
-    inc(table, coords, 2)
-  end
-
-  def act(:off, table, coords) do
-    dec(table, coords, -1)
-  end
-
-
-  def init do
-    :ets.new(__MODULE__, [:set])
-  end
-
-
-  def cleanup(table) do
-    :ets.delete(table)
-  end
-
-
-  def inc(table, coords, amount) do
-    :ets.update_counter(table, coords, amount, {coords, 0})
-  end
-
-
-  def dec(table, coords, amount) do
-    :ets.update_counter(table, coords, {2, amount, 0, 0}, {coords, 0})
-  end
-
-
-  def sum(table) do
-    :ets.foldl(fn {_, n}, acc -> acc + n end, 0, table)
-  end
 
 end
